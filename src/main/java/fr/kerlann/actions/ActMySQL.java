60e3aa8eed0ff30c3858ccaf8cc8a370570c1cd5
Kerlann <kerlann.pro@gmail.com>
2021-08-26 15:27:25 +0200

    update sqript

--- src/main/java/fr/kerlann/actions/ActMySQL.java
+++ src/main/java/fr/kerlann/actions/ActMySQL.java
@@ -1,6 +1,7 @@
 package fr.kerlann.actions;
 
 import fr.nico.sqript.actions.ScriptAction;
+import fr.nico.sqript.meta.Feature;
 import fr.nico.sqript.network.ScriptNetworkManager;
 import fr.nico.sqript.types.TypePlayer;
 import fr.nico.sqript.compiling.ScriptException;
@@ -13,12 +14,10 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 @Action(name = "MySQL Actions",
-        description ="MySQL related actions",
-        examples = {"execute test in {mysql}"
-        },
-        patterns = {
-                "mysql execute {string} in {mysql}"
+        features = {
+                @Feature(name = "Mysql execute", description = "Executing a Sql query", examples = "mysql execute \"INSERT INTO `user`(`player_name`, `coins`, `isHand`) VALUES ('%{username}%',%{coins}%,%{isHand}%)\" in ${MySQL}", pattern = "mysql execute {string} in {mysql}"),
         }
 )
 public class ActMySQL extends ScriptAction {
--- src/main/java/fr/kerlann/expressions/ExprMySQL.java
+++ src/main/java/fr/kerlann/expressions/ExprMySQL.java
@@ -6,23 +6,25 @@ import fr.kerlann.types.TypeResultSet;
 import fr.nico.sqript.ScriptManager;
 import fr.nico.sqript.expressions.ScriptExpression;
 import fr.nico.sqript.meta.Expression;
+import fr.nico.sqript.meta.Feature;
 import fr.nico.sqript.structures.ScriptContext;
 import fr.nico.sqript.types.ScriptType;
 import fr.nico.sqript.types.TypeArray;
 import fr.nico.sqript.types.TypeNull;
+import fr.nico.sqript.types.primitive.TypeBoolean;
 import fr.nico.sqript.types.primitive.TypeString;
 
 import javax.sql.rowset.CachedRowSet;
 import java.sql.*;
 import java.util.ArrayList;
 
-@Expression(name = "MySQL Expressions",
-        description = "Get informations about the system",
-        examples = "execution side",
-        patterns = {
-                "the database {string}",
-                "{mysql} result of query {string}",
-                "{mysql} string {string} in {resultset}",
+@Expression(name = "MySQL expressions",
+        features = {
+                @Feature(name = "Initialize database", description = "Iinitialize database", examples = "set ${MySQL} to the database \"jdbc:mysql://localhost:3306/test?user=root&password=\"", pattern = "the database {string}", type = "mysql"),
+                @Feature(name = "ResultSet database", description = "Retrieve information from the sql query", examples = "set {result} to ${MySQL} result of query \"SELECT * FROM `user` WHERE `player_name` = '\"+{username}+\"'\"", pattern = "{mysql} result of query {string}", type = "resultset"),
+                @Feature(name = "Value String ResultSet", description = "get value string", examples = "set {playerName_array} to ${MySQL} string \"player_name\" in {result}", pattern = "{mysql} string {string} in {resultset}", type = "array"),
+                @Feature(name = "Value Boolean ResultSet", description = "get value boolean", examples = "set {playerName_array} to ${MySQL} boolean \"isHand\" in {result}", pattern = "{mysql} boolean {string} in {resultset}", type = "array"),
+
         }
 )
 public class ExprMySQL extends ScriptExpression {
@@ -57,7 +59,7 @@ public class ExprMySQL extends ScriptExpression {
                         return new TypeNull();
                     }
                 } catch (SQLException ex) {
-                    ScriptManager.log.error("Error in " + getLine().scriptInstance.getName() + "- Line "+getLine().number, ex);
+                    ScriptManager.log.error("Error in " + getLine().getScriptInstance().getName() + "- Line "+getLine().getLineNumber(), ex);
                     return new TypeNull();
                 }
             case 2:
@@ -71,7 +73,21 @@ public class ExprMySQL extends ScriptExpression {
                     resultSet.release();
                     return new TypeArray(typeLists);
                 } catch (SQLException ex) {
-                    ScriptManager.log.error("Error in " + getLine().scriptInstance.getName() + "- Line " + getLine().number, ex);
+                    ScriptManager.log.error("Error in " + getLine().getScriptInstance().getName() + "- Line " + getLine().getLineNumber(), ex);
+                    return new TypeNull();
+                }
+            case 3:
+                conn = (Connection) parameters[0].getObject();
+                resultSet = (CachedRowSet) parameters[2].getObject();
+                ArrayList< TypeBoolean > typeBooleanLists = new ArrayList<>();
+                try {
+                    while (resultSet.next()) {
+                        typeBooleanLists.add(new TypeBoolean(resultSet.getBoolean((String) parameters[1].getObject())));
+                    }
+                    resultSet.release();
+                    return new TypeArray(typeBooleanLists);
+                } catch (SQLException ex) {
+                    ScriptManager.log.error("Error in " + getLine().getScriptInstance().getName() + "- Line " + getLine().getLineNumber(), ex);
                     return new TypeNull();
                 }
         }
